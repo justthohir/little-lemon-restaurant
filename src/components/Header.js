6b5a282a762d73c7e18a91efffcef4fecdf1ff3a
mthohirin11 <mthohirin11@gmail.com>
2024-06-10 20:49:57 +0700

    setup initial components

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>Header</h1>
+        <Nav />
+      </header>
+    </>
+  );
+};
+
+export default Header;
